88ba80fbf91b705927b3c96b0dcb5baaa336c1b8
husna <husna@DESKTOP-TL0QEH7>
2025-09-01 22:00:00 +0500

    Data added

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -5,31 +5,19 @@ export default function Splash() {
   const { t } = useTranslation();
   return (
     <div className="splash">
-      <div className="flex flex-col items-center w-[84%] max-w-md">
-        {/* Logo tile */}
-        <div className="splash-logo-tile">
+      <div className="flex flex-col items-center w-[86%] max-w-md">
+        {/* Animated logo */}
+        <div className="splash-logo-wrap">
           <img
             src="/sh-logo.png"
             alt={t("brand")}
-            className="splash-logo-img"
+            className="splash-logo-anim"
           />
         </div>
 
         {/* Brand & tagline */}
         <div className="splash-brand">{t("brand")}</div>
         <div className="splash-tagline">{t("splash_tagline")}</div>
-
-        {/* Road + moving truck (logo) */}
-        <div className="w-full mt-8">
-          <div className="road road-animate">
-            <div className="road-centerline" />
-            <img
-              src="/sh-logo.png"
-              alt="truck"
-              className="truck-animate"
-            />
-          </div>
-        </div>
       </div>
     </div>
   );
